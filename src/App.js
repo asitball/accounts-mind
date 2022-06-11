aaa9e0adb12f9d6fb70fb7e83d74b1faaba0a236
asitball <asit3310@gmail.com>
2022-06-11 21:50:29 +0600

    project setup

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import Cart from './components/Cart/Cart';
-import Player from './components/Player/Player';
-import playersData from './data/data.json';
+import React from 'react';
 
-function App() {
-  const [player, setPlayer] = useState([]);
-  const [cart, setCart] = useState([]);
-  useEffect(()=> {
-    setPlayer(playersData);
-  },[])
-  const handleAddPlayer = (player)=> {
-    const newCart = [...cart, player];
-     setCart(newCart);
-  }
+const App = () => {
   return (
-    <div className="container">
-       <div className="row">
-            <div className='col-md-8 player-container'>
-            {
-              playersData.map(player=> <Player player= {player} handleAddPlayer={handleAddPlayer}></Player>)
-            }
-          </div>
-          <div className="col-md-4 count-container">
-            <h3>Total Players: {player.length}</h3>
-            <h4>Player Added:  {cart.length}</h4>
-            <Cart cart={cart}></Cart>
-          </div>
-       </div>
+    <div>
+      <h1>project setup assignment-10</h1>
     </div>
   );
-}
+};
 
 export default App;
