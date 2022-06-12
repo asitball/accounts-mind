b757f1fcca8c920bb0370a155ad1f617107b1a19
asitball <asit3310@gmail.com>
2022-06-12 12:38:08 +0600

    banner and service card added

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -1,9 +1,57 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import { useState } from 'react';
+import banner1 from '../../../images/banner/banner-1.jpg'
+import banner2 from '../../../images/banner/banner-2.jpg'
+import banner3 from '../../../images/banner/banner-3.jpg'
 
 const Banner = () => {
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex, e) => {
+        setIndex(selectedIndex);
+    };
     return (
-        <div>
-            This is banner page
+        <div className='w-100%'>
+            <Carousel activeIndex={index} onSelect={handleSelect}>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={banner1}
+                        alt="First slide "
+                    />
+                    <Carousel.Caption>
+                        <h3>Financial Analysis</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={banner2}
+                        alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Investment Analysis</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={banner3}
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Auditing</h3>
+                        <p>
+                            Auditing typically refers to financial statement audits or an objective examination and evaluation of a company's financial statements
+                        </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
         </div>
     );
 };
---
+++ src/components/Home/Services/Services.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import Service from '../Service/Service';
+
+
+const Services = () => {
+    const [services, setServices] = useState([]);
+    useEffect(() =>{
+        fetch('services.json')
+        .then(res => res.json())
+        .then(data  => setServices(data))
+    },[])
+   
+    return (
+        <div className='container'>
+           <h3>total data: {services.length}</h3>
+          <div className='row g-4'>
+          {
+            services.map(service => <Service key={service.id} service={service}></Service>)
+           }
+          </div>
+        </div>
+    );
+};
+
+export default Services;
